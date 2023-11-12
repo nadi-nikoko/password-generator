9730160a92a4e74a181604c674effb124b878312
nico.nardi@ <plantacaotica@plantas-MBP.homenet.telecomitalia.it>
2023-11-12 13:02:12 +0100

    password generator

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+function generator(){
+    passwordOne.textContent = generatePassword();
+    passwordTwo.textContent = generatePassword();
+}
+
+function generatePassword (){
+    let password = "";
+    while (password.length < 15){
+        let randomNumber = Math.floor(Math.random() * characters.length);
+        password += characters[randomNumber];
+        console.log(password);
+    }
+    return password;
+}
